4efdb44c78102a9c2159ba85b36111a0cfe52f98
NGUYEN KIEU THANH CONG <1521050336@student.humg.edu.vn>
2018-03-02 09:50:43 +0700

    fdeg

--- QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/frmLogin.cs
+++ QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/frmLogin.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using DevExpress.XtraEditors;
-using Model.DAO;
+
 
 namespace QLHSBanTru2018_Demo_V1
 {
@@ -21,42 +21,7 @@ namespace QLHSBanTru2018_Demo_V1
 
         private void btnLogin_Click(object sender, EventArgs e)
         {
-            if (txtUsername.Text == "" || txtPassword.Text == "")
-            {
-                labError.Text = "Mời bạn nhập đầy đủ thông tin";
-                txtUsername.Focus();
-                txtPassword.ResetText();
-            }
-            else
-            {
-                int result = new EmployeeDAO().Login(txtUsername.Text, MD5Hash.PasswordEncryptor.MD5Hash(txtPassword.Text));
-                if (result == 0)
-                {
-                    labError.Text = "Tài khoản không tồn tại hoặc đã xảy ra lỗi";
-                    txtUsername.Focus();
-                    txtPassword.ResetText();
-                }
-                else
-                {
-                    if (result == 2)
-                    {
-                        labError.Text = "Mật khẩu không chính xác";
-                        txtPassword.ResetText();
-                        txtPassword.Focus();
-                    }
-                    if (result == -1)
-                    {
-                        labError.Text = "Tài khoản đã bị khoá";
-                        txtUsername.Focus();
-                        txtPassword.ResetText();
-                    }
-                    else if (result == 1)
-                    {
-                        DialogResult = DialogResult.OK;
-                        Close();
-                    }
-                }
-            }
+           
         }
 
         private void btnExit_Click(object sender, EventArgs e)
