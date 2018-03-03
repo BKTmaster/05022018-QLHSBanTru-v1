1b3478a147359f9a407df8d6bc58caf30a8c8338
duchung1510 <duchung.1510@gmail.com>
2018-03-03 10:50:38 +0700

    A

--- QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/Program.cs
+++ QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/Program.cs
@@ -36,9 +36,9 @@ namespace QLHSBanTru2018_Demo_V1
 
             BonusSkins.Register();
             SkinManager.EnableFormSkins();
-            //DialogResult showLogin = new frmLogin().ShowDialog();
-            //if (showLogin != DialogResult.OK) return;
-            //Application.Run(new frmMain());
+            DialogResult showLogin = new frmLogin().ShowDialog();
+            if (showLogin != DialogResult.OK) return;
+            Application.Run(new frmMain());
             Application.Run(new frmMain());
             
         }
