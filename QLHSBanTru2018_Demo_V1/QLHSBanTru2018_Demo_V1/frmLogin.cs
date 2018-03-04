aae77fd51d0cc92f67954a4d4b610cb01b1b9111
duchung1510 <duchung.1510@gmail.com>
2018-03-05 00:45:52 +0700

    A

--- QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/frmLogin.cs
+++ QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/frmLogin.cs
@@ -56,6 +56,6 @@ namespace QLHSBanTru2018_Demo_V1
         private void btnFacebook_Click(object sender, EventArgs e)
         {
             System.Diagnostics.Process.Start("http://facebook.com/duchung.1510");
-        }
+        }        
     }
 }
