976aa21e7a05899665de6cebe57b4ff3e4ae0229
TienBao202 <36029834+TienBao202@users.noreply.github.com>
2018-03-06 08:57:19 +0700

    Student

---
+++ QLHSBanTru2018_Demo_V1/QLHSBanTru2018_Demo_V1/QLHocSinh/frmHocSinh.Designer.cs
@@ -0,0 +1,335 @@
+﻿namespace QLHSBanTru2018_Demo_V1.QLHocSinh
+{
+    partial class frmHocSinh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHocSinh));
+            this.panelControl1 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl2 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl3 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl4 = new DevExpress.XtraEditors.PanelControl();
+            this.panelControl5 = new DevExpress.XtraEditors.PanelControl();
+            this.layoutControl1 = new DevExpress.XtraLayout.LayoutControl();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.SearchHocSinh = new DevExpress.XtraEditors.SearchControl();
+            this.cmbNamHoc = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.cmbKhoiHoc = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.cmbLopHoc = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.cmbTrangThai = new DevExpress.XtraEditors.ComboBoxEdit();
+            this.btnHuy = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXem = new DevExpress.XtraEditors.SimpleButton();
+            this.btnThem = new DevExpress.XtraEditors.SimpleButton();
+            this.btnSua = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXoa = new DevExpress.XtraEditors.SimpleButton();
+            this.btnNhapKhau = new DevExpress.XtraEditors.SimpleButton();
+            this.btnXuatKhau = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton6 = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton7 = new DevExpress.XtraEditors.SimpleButton();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).BeginInit();
+            this.panelControl1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl4)).BeginInit();
+            this.panelControl4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchHocSinh.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbNamHoc.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbKhoiHoc.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbLopHoc.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbTrangThai.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelControl1
+            // 
+            this.panelControl1.Controls.Add(this.btnHuy);
+            this.panelControl1.Controls.Add(this.cmbTrangThai);
+            this.panelControl1.Controls.Add(this.cmbLopHoc);
+            this.panelControl1.Controls.Add(this.cmbKhoiHoc);
+            this.panelControl1.Controls.Add(this.cmbNamHoc);
+            this.panelControl1.Controls.Add(this.SearchHocSinh);
+            this.panelControl1.Controls.Add(this.layoutControl1);
+            this.panelControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl1.Location = new System.Drawing.Point(0, 0);
+            this.panelControl1.Name = "panelControl1";
+            this.panelControl1.Size = new System.Drawing.Size(1058, 65);
+            this.panelControl1.TabIndex = 0;
+            // 
+            // panelControl2
+            // 
+            this.panelControl2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panelControl2.Location = new System.Drawing.Point(0, 467);
+            this.panelControl2.Name = "panelControl2";
+            this.panelControl2.Size = new System.Drawing.Size(1058, 40);
+            this.panelControl2.TabIndex = 1;
+            // 
+            // panelControl3
+            // 
+            this.panelControl3.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panelControl3.Location = new System.Drawing.Point(817, 65);
+            this.panelControl3.Name = "panelControl3";
+            this.panelControl3.Size = new System.Drawing.Size(241, 402);
+            this.panelControl3.TabIndex = 2;
+            // 
+            // panelControl4
+            // 
+            this.panelControl4.Controls.Add(this.simpleButton7);
+            this.panelControl4.Controls.Add(this.simpleButton6);
+            this.panelControl4.Controls.Add(this.btnXuatKhau);
+            this.panelControl4.Controls.Add(this.btnNhapKhau);
+            this.panelControl4.Controls.Add(this.btnXoa);
+            this.panelControl4.Controls.Add(this.btnSua);
+            this.panelControl4.Controls.Add(this.btnThem);
+            this.panelControl4.Controls.Add(this.btnXem);
+            this.panelControl4.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelControl4.Location = new System.Drawing.Point(0, 65);
+            this.panelControl4.Name = "panelControl4";
+            this.panelControl4.Size = new System.Drawing.Size(817, 61);
+            this.panelControl4.TabIndex = 3;
+            // 
+            // panelControl5
+            // 
+            this.panelControl5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panelControl5.Location = new System.Drawing.Point(0, 126);
+            this.panelControl5.Name = "panelControl5";
+            this.panelControl5.Size = new System.Drawing.Size(817, 341);
+            this.panelControl5.TabIndex = 4;
+            // 
+            // layoutControl1
+            // 
+            this.layoutControl1.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.layoutControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.layoutControl1.Location = new System.Drawing.Point(2, 2);
+            this.layoutControl1.Name = "layoutControl1";
+            this.layoutControl1.Root = this.layoutControlGroup1;
+            this.layoutControl1.Size = new System.Drawing.Size(1054, 26);
+            this.layoutControl1.TabIndex = 0;
+            this.layoutControl1.Text = "layoutControl1";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlGroup1.Name = "layoutControlGroup1";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(1054, 26);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // SearchHocSinh
+            // 
+            this.SearchHocSinh.EditValue = "-- Nhập từ khóa --";
+            this.SearchHocSinh.Location = new System.Drawing.Point(6, 35);
+            this.SearchHocSinh.Name = "SearchHocSinh";
+            this.SearchHocSinh.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.SearchHocSinh.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Repository.ClearButton(),
+            new DevExpress.XtraEditors.Repository.SearchButton()});
+            this.SearchHocSinh.Size = new System.Drawing.Size(209, 22);
+            this.SearchHocSinh.TabIndex = 1;
+            // 
+            // cmbNamHoc
+            // 
+            this.cmbNamHoc.EditValue = "-- Chọn năm học --";
+            this.cmbNamHoc.Location = new System.Drawing.Point(222, 35);
+            this.cmbNamHoc.Name = "cmbNamHoc";
+            this.cmbNamHoc.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.cmbNamHoc.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbNamHoc.Size = new System.Drawing.Size(142, 22);
+            this.cmbNamHoc.TabIndex = 2;
+            // 
+            // cmbKhoiHoc
+            // 
+            this.cmbKhoiHoc.EditValue = "-- Chọn khối học --";
+            this.cmbKhoiHoc.Location = new System.Drawing.Point(370, 34);
+            this.cmbKhoiHoc.Name = "cmbKhoiHoc";
+            this.cmbKhoiHoc.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.cmbKhoiHoc.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbKhoiHoc.Size = new System.Drawing.Size(134, 22);
+            this.cmbKhoiHoc.TabIndex = 3;
+            // 
+            // cmbLopHoc
+            // 
+            this.cmbLopHoc.EditValue = "-- Chọn lớp học --";
+            this.cmbLopHoc.Location = new System.Drawing.Point(510, 34);
+            this.cmbLopHoc.Name = "cmbLopHoc";
+            this.cmbLopHoc.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.cmbLopHoc.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbLopHoc.Size = new System.Drawing.Size(132, 22);
+            this.cmbLopHoc.TabIndex = 4;
+            // 
+            // cmbTrangThai
+            // 
+            this.cmbTrangThai.EditValue = "-- Chọn trạng thái --";
+            this.cmbTrangThai.Location = new System.Drawing.Point(648, 34);
+            this.cmbTrangThai.Name = "cmbTrangThai";
+            this.cmbTrangThai.Properties.BorderStyle = DevExpress.XtraEditors.Controls.BorderStyles.HotFlat;
+            this.cmbTrangThai.Properties.Buttons.AddRange(new DevExpress.XtraEditors.Controls.EditorButton[] {
+            new DevExpress.XtraEditors.Controls.EditorButton(DevExpress.XtraEditors.Controls.ButtonPredefines.Combo)});
+            this.cmbTrangThai.Size = new System.Drawing.Size(132, 22);
+            this.cmbTrangThai.TabIndex = 5;
+            // 
+            // btnHuy
+            // 
+            this.btnHuy.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("btnHuy.ImageOptions.Image")));
+            this.btnHuy.Location = new System.Drawing.Point(799, 33);
+            this.btnHuy.Name = "btnHuy";
+            this.btnHuy.Size = new System.Drawing.Size(63, 23);
+            this.btnHuy.TabIndex = 6;
+            this.btnHuy.Text = "Hủy";
+            // 
+            // btnXem
+            // 
+            this.btnXem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image1")));
+            this.btnXem.Location = new System.Drawing.Point(6, 6);
+            this.btnXem.Name = "btnXem";
+            this.btnXem.Size = new System.Drawing.Size(75, 34);
+            this.btnXem.TabIndex = 0;
+            this.btnXem.Text = "Xem";
+            // 
+            // btnThem
+            // 
+            this.btnThem.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton1.ImageOptions.Image")));
+            this.btnThem.Location = new System.Drawing.Point(87, 6);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(75, 34);
+            this.btnThem.TabIndex = 1;
+            this.btnThem.Text = "Thêm";
+            // 
+            // btnSua
+            // 
+            this.btnSua.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton2.ImageOptions.Image")));
+            this.btnSua.Location = new System.Drawing.Point(168, 6);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(75, 34);
+            this.btnSua.TabIndex = 2;
+            this.btnSua.Text = "Sửa";
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton3.ImageOptions.Image")));
+            this.btnXoa.Location = new System.Drawing.Point(249, 6);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(75, 34);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            // 
+            // btnNhapKhau
+            // 
+            this.btnNhapKhau.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton4.ImageOptions.Image")));
+            this.btnNhapKhau.Location = new System.Drawing.Point(330, 6);
+            this.btnNhapKhau.Name = "btnNhapKhau";
+            this.btnNhapKhau.Size = new System.Drawing.Size(100, 34);
+            this.btnNhapKhau.TabIndex = 4;
+            this.btnNhapKhau.Text = "Nhập khẩu";
+            // 
+            // btnXuatKhau
+            // 
+            this.btnXuatKhau.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton5.ImageOptions.Image")));
+            this.btnXuatKhau.Location = new System.Drawing.Point(436, 6);
+            this.btnXuatKhau.Name = "btnXuatKhau";
+            this.btnXuatKhau.Size = new System.Drawing.Size(106, 34);
+            this.btnXuatKhau.TabIndex = 5;
+            this.btnXuatKhau.Text = "Xuất khẩu";
+            // 
+            // simpleButton6
+            // 
+            this.simpleButton6.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton6.ImageOptions.Image")));
+            this.simpleButton6.Location = new System.Drawing.Point(620, 6);
+            this.simpleButton6.Name = "simpleButton6";
+            this.simpleButton6.Size = new System.Drawing.Size(75, 34);
+            this.simpleButton6.TabIndex = 6;
+            this.simpleButton6.Text = "Xem";
+            // 
+            // simpleButton7
+            // 
+            this.simpleButton7.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("simpleButton7.ImageOptions.Image")));
+            this.simpleButton7.Location = new System.Drawing.Point(701, 6);
+            this.simpleButton7.Name = "simpleButton7";
+            this.simpleButton7.Size = new System.Drawing.Size(75, 34);
+            this.simpleButton7.TabIndex = 7;
+            this.simpleButton7.Text = "Xem";
+            // 
+            // frmHocSinh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1058, 507);
+            this.Controls.Add(this.panelControl5);
+            this.Controls.Add(this.panelControl4);
+            this.Controls.Add(this.panelControl3);
+            this.Controls.Add(this.panelControl2);
+            this.Controls.Add(this.panelControl1);
+            this.Name = "frmHocSinh";
+            this.Text = "frmHocSinh";
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl1)).EndInit();
+            this.panelControl1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl4)).EndInit();
+            this.panelControl4.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.panelControl5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.SearchHocSinh.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbNamHoc.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbKhoiHoc.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbLopHoc.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.cmbTrangThai.Properties)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraEditors.PanelControl panelControl1;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbLopHoc;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbKhoiHoc;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbNamHoc;
+        private DevExpress.XtraEditors.SearchControl SearchHocSinh;
+        private DevExpress.XtraLayout.LayoutControl layoutControl1;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraEditors.PanelControl panelControl2;
+        private DevExpress.XtraEditors.PanelControl panelControl3;
+        private DevExpress.XtraEditors.PanelControl panelControl4;
+        private DevExpress.XtraEditors.PanelControl panelControl5;
+        private DevExpress.XtraEditors.SimpleButton btnHuy;
+        private DevExpress.XtraEditors.ComboBoxEdit cmbTrangThai;
+        private DevExpress.XtraEditors.SimpleButton simpleButton7;
+        private DevExpress.XtraEditors.SimpleButton simpleButton6;
+        private DevExpress.XtraEditors.SimpleButton btnXuatKhau;
+        private DevExpress.XtraEditors.SimpleButton btnNhapKhau;
+        private DevExpress.XtraEditors.SimpleButton btnXoa;
+        private DevExpress.XtraEditors.SimpleButton btnSua;
+        private DevExpress.XtraEditors.SimpleButton btnThem;
+        private DevExpress.XtraEditors.SimpleButton btnXem;
+    }
+}
